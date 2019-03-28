41d08f1501b730b3db5d443d793e7fc0bd0d738d
ManoharanGurusamy <mano.gurusamy01@gmail.com>
2019-03-28 14:47:57 +0000

    new comment added

--- src/user.js
+++ src/user.js
@@ -2,3 +2,5 @@ let userName = 'Manoharan Gurusamy';
 const greetUser = (user = userName) => `Hello ${user}`;
 console.log(greetUser());
 console.log(greetUser('Bindhu'));
+
+// Assume that this was updated whle we develop our future branch.
