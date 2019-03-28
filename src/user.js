406e5e07b541bcab9d17b43373180425a54ef273
Manoharan Gurusamy <mgurusamy@virtusa.com>
2019-03-28 16:26:11 +0000

    Functional Programming aspects added

--- src/user.js
+++ src/user.js
@@ -4,3 +4,47 @@ console.log(greetUser());
 console.log(greetUser('Bindhu'));
 
 // Assume that this was updated while we develop our future branch.
+const members = [
+  {
+    firstName: 'Manoharan',
+    lastName: 'Gurusamy',
+    sex: 'male',
+    profession: 'IT Consultant',
+    officeCommute: true,
+    adult: true
+  },
+  {
+    firstName: 'Bhagyalakshmi',
+    lastName: 'Balu',
+    sex: 'female',
+    profession: 'Catering Consultant',
+    officeCommute: false,
+    adult: true
+  },
+  {
+    firstName: 'Rhiya',
+    lastName: 'Manoharan',
+    sex: 'female',
+    profession: 'Student',
+    officeCommute: false,
+    adult: false
+  },
+  {
+    firstName: 'Guru Aditya',
+    lastName: 'Manoharan',
+    sex: 'male',
+    profession: 'Student',
+    officeCommute: false,
+    adult: false
+  }
+];
+
+console.log(
+  'List of adult Members',
+  members.filter(member => member.adult === true)
+);
+
+console.log(
+  'List of members fullName',
+  members.map(member => `${member.firstName} ${member.lastName}`)
+);
